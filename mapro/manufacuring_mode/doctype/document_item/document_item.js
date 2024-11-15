dfee9e818829aa73d31c528734fa064c03bb40ea
bestvikas <vikas.deshmukh@erpdata.in>
2024-11-15 12:42:46 +0530

    Addition

---
+++ mapro/manufacuring_mode/doctype/document_item/document_item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Pradip and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Document Item', {
+	// refresh: function(frm) {
+
+	// }
+});
